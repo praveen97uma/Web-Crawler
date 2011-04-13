3bba4fedc9355b06881941b92b970906a2a26e21
Praveen <praveen97uma@gmail.com>
2011-04-13 23:43:29 +0530

    Fixed Style issues

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 #!usr/bin/env python
+
 from sys import argv
 from collections import deque
 from retriever import Retriever
@@ -6,67 +7,72 @@ from urlparse import urlparse
 from time import ctime
 from downloader import Downloader
 from os.path import splitext
-
+import logging
 class Crawler(object):
-    "crawler goes out to the web and downloads the web pages"
-    _invalidExt=['.pdf','jpg','jpeg','.doc','docx','.gif','.zip','.rar','.PDF']
+    """crawler goes out to the web and downloads the web pages
+    """
+    _invalidExt = ['.pdf','jpg','jpeg','.doc','docx','.gif','.zip','.rar','.PDF']
     def __init__(self):
-        self.visited_links=[]
-        self.links_queue=deque([])
-        self.domain=''
-        self.same_domain=True
-      
+        self.visited_links = []
+        self.links_queue = deque([])
+        self.domain = ''
+        self.same_domain = True
+        logging.basicConfig(filename = 'crawler.log', format = '%(levelname)s:%(message)s', level = logging.INFO)
+
+         
             
-    def crawlPage(self,url,same_domain=True):
-        pageRetriever=Retriever()
-        downloader=Downloader()
-        retrieverResponse=downloader.download(url)
+    def crawlPage(self, url, same_domain = True):
+        pageRetriever = Retriever()
+        downloader = Downloader()
+        retrieverResponse = downloader.CDownload(url)
         
-        if retrieverResponse==0:
-            print retrieverResponse,"Invalid Url.....parsing skipped\n"
+        if retrieverResponse == 0:
+            print retrieverResponse, "Invalid Url.....parsing skipped\n"
             return
         
         self.visited_links.append(url)
         
         try:
-            links=pageRetriever.getLinks(url)
+            links = pageRetriever.getLinks(url)
         except Exception:
             return
    
         
         for link in links:
             if link not in self.visited_links:
-                if same_domain==True:
-                    if urlparse(link)[1]!=self.domain:
-                        print link," *** discarded for crawl .. not in domain\n"
-                        pageRetriever.log.write("%s %s*** discarded for crawl .. not in domain\n"%(ctime(),link))
+                if same_domain == True:
+                    if urlparse(link)[1] != self.domain:
+                        print link, " *** discarded for crawl .. not in domain"
+                        logging.info("%s * discarded for crawl .. not in domain"%link)
                     else:
                         if link not in self.links_queue:
                             if splitext(link)[1] not in self._invalidExt:
                                 self.links_queue.append(link)
-                                print link," *** new link added to crawl queue\n"
-                                pageRetriever.log.write("%s %s *** new link added to crawl queue\n"%(ctime(),link))
+                                print link, " *** new link added to crawl queue"
+                                logging.info("%s * new link added to crawl queue"%link)
                         else:
                             print link,"*** discarded already visited"
-                            pageRetriever.log.write("%s %s *** discarded already visited\n"%(ctime(),link))
+                            logging.info("%s * discarded already visited"%link)
                     
-                if same_domain==False:
+                if same_domain == False:
                     if link not in self.links_queue:
                             self.links_queue.append(link)
-                            print link," *** new link added to crawl queue\n"
-                            pageRetriever.log.write("%s *** new link added to crawl queue\n"%link)
+                            print link," *** new link added to crawl queue"
+                            logging.info("%s * new link added to crawl queue"%link)
                     else:
                         print link,"*** discarded already visited"
-                        pageRetriever.log.write("%s *** discarded already visited"%link)
+                        logging.info("%s *** discarded already visited"%link)
                       
-        print "lemgth of queue is ",len(self.links_queue), "len of visited queue is ",len(self.visited_links)
-        pageRetriever.log.write("\n\nlength of queue is %d   length of visited queue is %d\n\n"%(len(self.links_queue),len(self.visited_links)))            
+        print "length of queue is ", len(self.links_queue), "len of visited queue is ",\
+            len(self.visited_links)
+        logging.info("length of queue is %d   length of visited queue is %d"\
+            %(len(self.links_queue),len(self.visited_links)))            
                     
                     
-    def start_crawl(self,url,same_domain=True):
+    def start_crawl(self, url, same_domain = True):
         self.links_queue.append(url)
-        self.domain=urlparse(url)[1] 
-        self.same_domain=same_domain              # process links in queue
+        self.domain = urlparse(url)[1] 
+        self.same_domain = same_domain              # process links in queue
         while self.links_queue:
             url = self.links_queue.popleft()
             self.crawlPage(url)
