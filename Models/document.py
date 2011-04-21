8e8d739b330e5d56dcd71c6ffbc5a3a09130d1d1
Praveen <praveen97uma@gmail.com>
2011-04-21 18:48:00 +0530

    Add new attributes to Document class.

--- Models/document.py
+++ Models/document.py
@@ -1,31 +1,35 @@
 #!usr/bin/python
 from hashlib import md5 
 class Document(object):
-   """Document abstract class
-   """
-   def __init__(self,url):
-      self.key=md5(url).hexdigest()
-      self.url=url
-      self.incomingLinks={}
-      self.outgoingLinks={}
-      self.numberofIL=0
-      self.numberofOL=0
-      self.docType=''
+    """Document abstract class
+    """
+    def __init__(self,url):
+        self.key = md5(url).hexdigest()
+        self.url = url
+        self.incomingLinks = {}
+        self.outgoingLinks = {}
+        self.numberofIL = 0
+        self.numberofOL = 0
+        self.docType = ''
+        self.all_terms = []
+        self.unique_terms_freq = {}
       
-   def insertIL(self,link):
-      """method to insert incoming or outgoing links into the document
-      """
-      if link not in self.incomingLinks.itervalues():
-         self.incomingLinks[md5(link).hexdigest()]=link
-         self.numberofIL+=1
+    def insertIL(self,link):
+        """
+        Method to insert incoming or outgoing links into the document
+        """
+        if link not in self.incomingLinks.itervalues():
+            self.incomingLinks[md5(link).hexdigest()]=link
+            self.numberofIL+=1
       
-   def insertOL(self,links):
-      """method to insert an outgoing link
-      """
-      #print links
-      self.outgoingLinks=dict([(md5(url).hexdigest(),url) for url in links])
-      #print self.outgoingLinks
-      self.numberofOL+=len(links)
+    def insertOL(self,links):
+        """
+        Method to insert an outgoing link
+        """
+        #print links
+        self.outgoingLinks=dict([(md5(url).hexdigest(),url) for url in links])
+        #print self.outgoingLinks
+        self.numberofOL+=len(links)
       
       
    
